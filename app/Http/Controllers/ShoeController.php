c0d4354e230b11c019286f75dbc66838c7a568ef
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-19 10:24:13 +0000

    create shoeController api with crud methods

--- app/Http/Controllers/ShoeController.php
+++ app/Http/Controllers/ShoeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Shoe;
 use Illuminate\Http\Request;
 
 class ShoeController extends Controller
@@ -11,7 +12,9 @@ class ShoeController extends Controller
      */
     public function index()
     {
-        //
+        $shoes = Shoe::all();
+
+        return response()->json($shoes);
     }
 
     /**
@@ -19,30 +22,61 @@ class ShoeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validated = $request->validate([
+                'name' => 'required|string',
+                'size' => 'required|integer',
+                'color' => 'required|string',
+                'price' => 'required|numeric',
+            ]);
+            $shoe = Shoe::create($validated);
+
+            return response()->json($shoe, 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Shoe $shoe)
     {
-        //
+        return response()->json($shoe, 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Shoe $shoe)
     {
-        //
+        try {
+            $validated = $request->validate([
+                'name' => 'required|string',
+                'size' => 'required|integer',
+                'color' => 'required|string',
+                'price' => 'required|numeric',
+            ]);
+
+            $shoe->update($validated);
+
+            return response()->json($shoe, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Shoe $shoe)
     {
-        //
+        try {
+            $shoe->delete();
+
+            return response()->json(['message' => 'shoe deleted'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
