08c8ae1bcd1f7a471b311f9d156ccb322b925f44
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-03-19 10:26:39 +0000

    Add routes for api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ShoeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('shoes', ShoeController::class);
